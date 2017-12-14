b956c69fe340ea4cae3845ca68122af193849fde
Rica Duchateau <rica.duchateau@informed.com>
2017-12-14 18:55:54 +0000

    Docker entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create database migration files
+echo "Create database migration files"
+python manage.py makemigrations application
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
