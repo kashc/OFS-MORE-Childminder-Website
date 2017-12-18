ab4a5e9e9cb672863fe0aac36436ba625840627a
Rica Duchateau <rica.duchateau@informed.com>
2017-12-18 13:52:38 +0000

    Personal Details logic unit testing

--- application/test_business_logic.py
+++ application/test_business_logic.py
@@ -10,7 +10,7 @@ from django.test import Client
 from django.test import TestCase
 from django.urls import resolve
 
-from .models import Application, Childcare_Type, Login_And_Contact_Details
+from .models import Application, Applicant_Names, Applicant_Personal_Details, Childcare_Type, Login_And_Contact_Details
 
 from uuid import UUID
 
@@ -37,11 +37,11 @@ class Test_Childcare_Type_Logic(TestCase):
         test_application_id = 'f8c42666-1367-4878-92e2-1cee6ebcb48c'
         
         # Delete test Childcare_Type object if it already exists
-        Childcare_Type.objects.filter(application_id='f8c42666-1367-4878-92e2-1cee6ebcb48c').delete()
+        Childcare_Type.objects.filter(application_id=test_application_id).delete()
         
         # Create a test application
         Application.objects.create(
-            application_id = (UUID('f8c42666-1367-4878-92e2-1cee6ebcb48c')),   
+            application_id = (UUID(test_application_id)),   
             login_details_status = 'NOT_STARTED',
             personal_details_status = 'NOT_STARTED',
             childcare_type_status = 'NOT_STARTED',
@@ -98,11 +98,11 @@ class Test_Login_Contact_Logic(TestCase):
         test_application_id = 'f8c42666-1367-4878-92e2-1cee6ebcb48c'
         
         # Delete test Login_And_Contact_Details object if it already exists
-        Login_And_Contact_Details.objects.filter(application_id='f8c42666-1367-4878-92e2-1cee6ebcb48c').delete()
+        Login_And_Contact_Details.objects.filter(application_id=test_application_id).delete()
         
         # Create a test application
         Application.objects.create(
-            application_id = (UUID('f8c42666-1367-4878-92e2-1cee6ebcb48c')),   
+            application_id = (UUID(test_application_id)),   
             login_details_status = 'NOT_STARTED',
             personal_details_status = 'NOT_STARTED',
             childcare_type_status = 'NOT_STARTED',
@@ -127,11 +127,93 @@ class Test_Login_Contact_Logic(TestCase):
             add_phone_number = ''
         )
         
-        # Verify that the Login_And_Contact_Detilas object corresponding with the test application exists
+        # Verify that the Login_And_Contact_Details object corresponding with the test application exists
         assert(Login_And_Contact_Details.objects.filter(application_id=test_application_id).count() > 0)
     
     # Delete test application
     def delete(self):
         
         Login_And_Contact_Details.objects.filter(application_id='f8c42666-1367-4878-92e2-1cee6ebcb48c').delete()
+        Application.objects.filter(application_id='f8c42666-1367-4878-92e2-1cee6ebcb48c').delete()
+
+
+# Test business logic to create or update a Your Personal Details record
+class Test_Personal_Logic(TestCase):
+    
+    # Test the business case where a new record needs to be created
+    def test_logic_to_create_new_record(self):
+        
+        # Create a test application ID
+        test_application_id = 'f8c42666-1367-4878-92e2-1cee6ebcb48c'
+        
+        # Create a test personal detail ID
+        test_personal_detail_id = '166f77f7-c2ee-4550-9461-45b9d2f28d34'
+        
+        # Delete test Applicant_Personal_Details and Applicant_Names objects if they already exist
+        Applicant_Personal_Details.objects.filter(application_id=test_application_id).delete()
+        Applicant_Names.objects.filter(personal_detail_id=test_personal_detail_id).delete()
+        
+        # Verify that the Applicant_Personal_Details and Applicant_Names objects corresponding with the test application do not exist
+        assert(Applicant_Personal_Details.objects.filter(application_id=test_application_id).count() == 0)
+        assert(Applicant_Names.objects.filter(personal_detail_id=test_personal_detail_id).count() == 0)
+    
+    # Test the business case where a record already exists
+    def test_logic_to_update_record(self):
+        
+        # Create a test application ID
+        test_application_id = 'f8c42666-1367-4878-92e2-1cee6ebcb48c'
+
+        # Create a test personal detail ID
+        test_personal_detail_id = '166f77f7-c2ee-4550-9461-45b9d2f28d34'
+        
+        # Delete test Applicant_Personal_Details and Applicant_Names objects if they already exist
+        Applicant_Personal_Details.objects.filter(application_id=test_application_id).delete()
+        Applicant_Names.objects.filter(personal_detail_id=test_personal_detail_id).delete()
+        
+        # Create a test application
+        Application.objects.create(
+            application_id = (UUID(test_application_id)),   
+            login_details_status = 'NOT_STARTED',
+            personal_details_status = 'NOT_STARTED',
+            childcare_type_status = 'NOT_STARTED',
+            first_aid_training_status = 'NOT_STARTED',
+            eyfs_training_status = 'NOT_STARTED',
+            criminal_record_check_status = 'NOT_STARTED',
+            health_status = 'NOT_STARTED',
+            references_status = 'NOT_STARTED',
+            people_in_home_status = 'NOT_STARTED',
+            declarations_status = 'NOT_STARTED',
+        )
+        
+        # Create a test Applicant_Personal_Details object
+        Applicant_Personal_Details.objects.create(
+            personal_detail_id = (UUID(test_personal_detail_id)),
+            application_id = Application.objects.get(application_id=test_application_id),
+            birth_day = 00,
+            birth_month = 00,
+            birth_year = 0000
+        )
+        
+        # Create a test name ID
+        test_name_id = '6e09fe41-2b07-4177-a5e4-347b2515ea8e'
+        
+        # Create a test Applicant_Names object
+        Applicant_Names.objects.create(
+            name_id = (UUID(test_name_id)),
+            personal_detail_id = Applicant_Personal_Details.objects.get(personal_detail_id=test_personal_detail_id),
+            current_name = 'True',
+            first_name = 'Erik',
+            middle_names = 'Tolstrup',
+            last_name = 'Odense'
+        )
+        
+        # Verify that the Applicant_Personal_Details and Applicant_Names objects corresponding with the test application exist
+        assert(Applicant_Personal_Details.objects.filter(application_id=test_application_id).count() > 0)
+        assert(Applicant_Names.objects.filter(personal_detail_id=test_personal_detail_id).count() > 0)
+    
+    # Delete test application
+    def delete(self):
+        
+        Applicant_Names_Details.objects.filter(name_id='6e09fe41-2b07-4177-a5e4-347b2515ea8e').delete()
+        Applicant_Personal_Details.objects.filter(personal_detail_id='166f77f7-c2ee-4550-9461-45b9d2f28d34').delete()
         Application.objects.filter(application_id='f8c42666-1367-4878-92e2-1cee6ebcb48c').delete()
